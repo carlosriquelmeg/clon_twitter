16b31b2dc6ba019438b4e1a3b6d6073ecdf8ae1f
Carlos <carlosriquelmegodoy@gmail.com>
2023-06-24 00:00:07 -0400

    se agrega gema buscador + bootstrap

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,14 @@
 class Tweet < ApplicationRecord
+    include PgSearch::Model
+    pg_search_scope :search_full_text, against: {
+        description: 'A',
+        username: 'B',
+},
+using: {
+    tsearch: { 
+        prefix: true,
+        any_word: true,
+        dictionary:'spanish'
+    }
+}
 end
